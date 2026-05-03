90e42b760669b197319f69dc9be7268f241ce674
Divyanshu <dnd.mein@gmail.com>
2026-05-03 16:12:18 +0530

    Refactor theme configuration and add header/footer navigation components

---
+++ components/common/header.tsx
@@ -0,0 +1,37 @@
+import { FileText } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import NavLink from "./nav-link";
+
+export default function Header() {
+  const isLoggedIn = false;
+  return (
+    <nav className="container flex items-center justify-between py-2 lg:px-8 px-2 mx-auto">
+      <div className="flex">
+        <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+          <FileText className="w-5 h-5  lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />
+          <span className="font-extrabold lg:text-xl text-gray-900">
+            Brevio
+          </span>
+        </NavLink>
+      </div>
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
+        <NavLink href="/#pricing">Pricing</NavLink>
+        {isLoggedIn &&<NavLink href="/dashboard">Your Summaries</NavLink>}
+      </div>
+      <div className="flex lg:justify-end ">
+        {isLoggedIn ? (
+          <div className="flex gap-2 items-center">
+            <NavLink href="/upload">Upload a PDF</NavLink>
+            <div className="">Pro</div>
+            <Button>User</Button>
+          </div>
+        ) : (
+          <div className="">
+            <NavLink href="/sign-in">Sign In</NavLink>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
